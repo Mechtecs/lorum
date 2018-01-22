dd1c7d5331eae2bd6c08f1293bc324f6995239cc
Max Schorradt <mechtecs@gmail.com>
2018-01-22 22:32:22 +0100

    Refactor, send id back

--- project/app/Http/Controllers/ServerGroupController.php
+++ project/app/Http/Controllers/ServerGroupController.php
@@ -9,65 +9,63 @@ use Illuminate\Support\Facades\Auth;
 
 class ServerGroupController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return ServerGroup[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
-     */
-    public function index()
-    {
-        if (!Auth::guest() && Auth::user()->hasRole("admin")) {
-            return ServerGroup::all()->sortBy('order')->load("servers");
-        } else {
-            return ServerGroup::whereActive(true)->get()->sortBy('order')->load("servers");
-        }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return ServerGroup[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
+   */
+  public function index() {
+    if (!Auth::guest() && Auth::user()->hasRole("admin")) {
+      return ServerGroup::all()->sortBy('order')->load("servers");
+    } else {
+      return ServerGroup::whereActive(true)->get()->sortBy('order')->load("servers");
     }
+  }
 
-    /**
-     * Create the new resource
-     *
-     * @param Request $request
-     * @return void
-     */
-    public function create(Request $request)
-    {
-        $serverGroup = new ServerGroup();
-        $serverGroup->fill($request->toArray());
-        $serverGroup->saveOrFail();
-        return $serverGroup;
-    }
+  /**
+   * Create the new resource
+   *
+   * @param Request $request
+   * @return void
+   */
+  public function create(Request $request) {
+    $serverGroup = new ServerGroup();
+    $serverGroup->fill($request->toArray());
+    $serverGroup->saveOrFail();
+    return $serverGroup;
+  }
 
-    /**
-     * Update the resource
-     *
-     * @param  \Illuminate\Http\Request $request
-     */
-    public function update(Request $request, ServerGroup $serverGroup)
-    {
-        $serverGroup->fill($request->toArray());
-        $serverGroup->saveOrFail();
-        return $serverGroup;
-    }
+  /**
+   * Update the resource
+   *
+   * @param  \Illuminate\Http\Request $request
+   */
+  public function update(Request $request, ServerGroup $serverGroup) {
+    $serverGroup->fill($request->toArray());
+    $serverGroup->save();
+    return $serverGroup;
+  }
 
-    /**
-     * Return the resource
-     * @param ServerGroup $server
-     */
-    public function get(ServerGroup $serverGroup)
-    {
-        $serverGroup->load("servers");
-    }
+  /**
+   * Return the resource
+   * @param ServerGroup $server
+   */
+  public function get(ServerGroup $serverGroup) {
+    $serverGroup->load("servers");
+    return $serverGroup;
+  }
 
-    /**
-     * Delete the resource
-     * @param ServerGroup $server
-     */
-    public function delete(ServerGroup $serverGroup)
-    {
-        if ($serverGroup->delete()) {
-            return ["success" => true];
-        } else {
-            return ["success" => false];
-        }
+  /**
+   * Delete the resource
+   * @param ServerGroup $server
+   */
+  public function delete(ServerGroup $serverGroup) {
+    $id = $serverGroup->id;
+    $serverGroup->servers()->delete();
+    if ($serverGroup->delete()) {
+      return ["success" => true, "id" => $id];
+    } else {
+      return ["success" => false, "id" => $id];
     }
+  }
 }
